e603f0c884643c629cfa14e491367bdd90323785
nilsedgar <nils.jacobsen@zocom.se>
2025-03-21 09:36:13 +0100

    Initial commit

---
+++ src/main/java/com/nilsedgar/dockerexercise/DockerExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.nilsedgar.dockerexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerExerciseApplication.class, args);
+	}
+
+}
